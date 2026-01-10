8889635677bcf7f520503370e9359b237062cd0f
apdev176 <rjstud176@gmail.com>
2026-01-10 17:32:07 +0530

    project setup

---
+++ src/main/java/com/nium/virtual/card/service/VirtualCardServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nium.virtual.card.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualCardServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualCardServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nium/virtual/card/service/VirtualCardServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nium.virtual.card.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualCardServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
